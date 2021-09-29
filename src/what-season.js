729d1dae6bddd6e7318c4477e2af8d7086f3d59f
Yulia Lepner <ylepner@gmail.com>
2021-09-29 23:05:52 +0200

    feat: complete what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,10 @@
 import { NotImplementedError } from '../extensions/index.js';
+const dictionary = {
+  0: 'winter',
+  1: 'spring',
+  2: 'summer',
+  3: 'autumn'
+}
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,7 +17,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+
+  if (!(date instanceof Date)) {
+    throw new Error('Invalid date!')
+  }
+  if (typeof date.getMonth !== 'function') {
+    throw new Error('Invalid date!')
+  }
+  try {
+    date.UTC()
+  } catch {
+    throw new Error('Invalid date!')
+  }
+  let seasonNumber = Math.floor(((date.getMonth() + 1) % 12) / 3)
+  return dictionary[seasonNumber]
 }
+
+console.log(getSeason(new Date(2020, 5, 31)))
