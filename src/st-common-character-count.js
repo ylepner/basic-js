f2e61fa3712afc03fbaf9045b5e4fe93bc65d27c
Yulia Lepner <ylepner@gmail.com>
2021-10-01 19:43:02 +0200

    feat: complete st-common-character-count

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -1,5 +1,15 @@
 import { NotImplementedError } from '../extensions/index.js';
 
+function createDict(string) {
+  let dict = {}
+  for (let letter of string) {
+    if (!dict[letter]) {
+      dict[letter] = 0
+    }
+    dict[letter] += 1
+  }
+  return dict
+}
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -11,7 +21,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  let dictS1 = createDict(s1)
+  let dictS2 = createDict(s2)
+  let count = 0
+  for (let key in dictS1) {
+    if (dictS2[key]) {
+      count += Math.min(dictS1[key], dictS2[key])
+    }
+  }
+  return count
 }
