de8217a287fd4467cf93c6b39b515890b6f40724
Yulia Lepner <ylepner@gmail.com>
2021-10-01 14:16:49 +0200

    feat: complete carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false
+  }
+  let sampleActivityNumber = Number(sampleActivity)
+  let result = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityNumber) / (Math.LN2 / HALF_LIFE_PERIOD));
+  if ((result >= 0) && (result !== Infinity)) {
+    return result;
+  }
+  return false;
 }
+
