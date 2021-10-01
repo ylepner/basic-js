aa57dbb4d0aa643a2cd1dde8fca301a1df1fd8f2
Yulia Lepner <ylepner@gmail.com>
2021-10-01 14:59:53 +0200

    feat: test recursive-depth

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,6 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+
 }
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,14 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 0;
+    for (el of arr) {
+      count += 1
+      if (Array.isArray(el)) {
+        count += deepCount(el)
+      }
+    }
+    return count
   }
 }
