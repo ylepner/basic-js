b1788031033d4d83f9cd2c2279f739351bc183a2
Yulia Lepner <ylepner@gmail.com>
2021-10-03 15:23:15 +0200

    feat: complete encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -11,6 +11,19 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function encodeLine(str) {
+  let newString = '';
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count += 1
+    } else {
+      if (count === 1) {
+        newString += str[i]
+      } else {
+        newString += count += str[i]
+        count = 1
+      }
+    }
+  }
+  return newString
 }
-
-console.log(encodeLine('aabbbc'))
