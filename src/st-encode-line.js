107187a2178877b5b73962b3702ff86a9dbfb478
Yulia Lepner <ylepner@gmail.com>
2021-10-03 13:50:19 +0200

    feat: complete email-domain

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let dict = {}
+  for (let letter of str) {
+    if (dict[letter]) {
+      dict[letter] += 1
+    }
+    dict[letter] = 0
+  }
+  return dict
 }
+
+console.log(encodeLine('aabbbc'))
--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -10,7 +10,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-export default function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getEmailDomain(email) {
+  for (let i = email.length; i >= 0; i--) {
+    if (email[i] === '@') {
+      return email.substr(i + 1);
+    }
+  }
 }
+
+console.log(getEmailDomain('very.unusual.@.unusual.com@usual.com'))
