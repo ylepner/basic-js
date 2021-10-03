20375bd4b3683df460cf6ffa9f342abfac3018e0
Yulia Lepner <ylepner@gmail.com>
2021-10-03 14:03:59 +0200

    fix: delete encode-line solution

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -11,14 +11,6 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function encodeLine(str) {
-  let dict = {}
-  for (let letter of str) {
-    if (dict[letter]) {
-      dict[letter] += 1
-    }
-    dict[letter] = 0
-  }
-  return dict
 }
 
 console.log(encodeLine('aabbbc'))
